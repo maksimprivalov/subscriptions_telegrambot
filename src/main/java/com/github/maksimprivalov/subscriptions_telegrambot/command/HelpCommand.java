5762ae5bc541f4ad0870746f5268bde1087533a7
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-09-27 16:30:41 +0200

    STEP_6 TgBot-5: added ability to subscribe on group TgBot-6: added ability to see the list of the group subscription.

--- src/main/java/com/github/maksimprivalov/subscriptions_telegrambot/command/HelpCommand.java
+++ src/main/java/com/github/maksimprivalov/subscriptions_telegrambot/command/HelpCommand.java
@@ -12,8 +12,12 @@ public class HelpCommand implements  Command{
     public static final String HELP_MESSAGE = String.format("✨<b>Available commands</b>✨\n\n"
     + "<b> Start \\ stop working with bot</b>\n" +
             "%s - start working with me ✨\n" +
-            "%s - pause working with me\n" +
-            "%s - get some helpful manuals about working with me!\n", START.getCommandName(), STOP.getCommandName(), HELP.getCommandName());
+            "%s - pause working with me\n\n" +
+            "Subscriptions: \n" +
+            "%s - subscribe a group \n" +
+            "%s - check my subscriptions \n\n" +
+            "%s - get some helpful manuals about working with me!\n", START.getCommandName(), STOP.getCommandName(),
+            ADD_GROUP_SUB.getCommandName(), LIST_GROUP_SUB.getCommandName(), HELP.getCommandName());
 
     public HelpCommand(SendBotMessageService sendBotMessageService) {
         this.sendBotMessageService = sendBotMessageService;
