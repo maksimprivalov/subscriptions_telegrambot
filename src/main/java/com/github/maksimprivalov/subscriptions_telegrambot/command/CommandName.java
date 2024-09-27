08da090573c41340bf09f68ed316a61157e0f5c8
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-09-27 16:57:21 +0200

    Little bug solved (listgroupsub)

--- src/main/java/com/github/maksimprivalov/subscriptions_telegrambot/command/CommandName.java
+++ src/main/java/com/github/maksimprivalov/subscriptions_telegrambot/command/CommandName.java
@@ -6,7 +6,8 @@ public enum CommandName {
     HELP("/help"),
     NO("NO"),
     STAT("/stat"),
-    ADD_GROUP_SUB("/addgroupsub");
+    ADD_GROUP_SUB("/addgroupsub"),
+    LIST_GROUP_SUB("/listgroupsub");
     private final String commandName;
     CommandName(String commandName){
         this.commandName = commandName;
